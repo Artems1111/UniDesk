b4b71de2ad6a82c70263dfa655c6a4d6966e3615
Apostolos Chalis <tolishalis@gmail.com>
2026-09-08 15:01:05 +0300

    Show icon in Unidesk bar

--- src/unidesk/main.py
+++ src/unidesk/main.py
@@ -7,16 +7,29 @@ from .home import UniOSWelcome
 
 def main():
     app = QApplication(sys.argv)
-    app.setApplicationName("UniDesk")
+    app.setApplicationName("unidesk")
+    app.setDesktopFileName("unidesk")
 
     current_dir = os.path.dirname(os.path.abspath(__file__))
 
-    icon_path = os.path.abspath(
-        os.path.join(current_dir, "..", "..", "resources", "unios.png")
-    )
-    app.setWindowIcon(QIcon(icon_path))
+    system_icon = "/usr/share/pixmaps/unidesk.png"
+
+    local_icon_1 = os.path.abspath(os.path.join(current_dir, "..", "resources", "unios.png"))
+    local_icon_2 = os.path.abspath(os.path.join(current_dir, "..", "..", "resources", "unios.png"))
+
+    if os.path.exists(system_icon):
+        app_icon = QIcon(system_icon)
+    elif os.path.exists(local_icon_1):
+        app_icon = QIcon(local_icon_1)
+    elif os.path.exists(local_icon_2):
+        app_icon = QIcon(local_icon_2)
+    else:
+        app_icon = QIcon.fromTheme("unidesk", QIcon.fromTheme("preferences-desktop"))
+
+    app.setWindowIcon(app_icon)
 
     window = UniOSWelcome()
+    window.setWindowIcon(app_icon)
     window.show()
     sys.exit(app.exec())
 
